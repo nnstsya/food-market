d265bd5b7831216311df42965e8acc1e42d8e67d
nnstsya <tonastyaket@gmail.com>
2025-01-07 13:41:45 +0200

    resolved comment, changed width of page, added padding to the bottom according to design

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
-import { HomepageComponent } from './homepage/homepage.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 const routes: Routes = [
   {
@@ -10,7 +10,7 @@ const routes: Routes = [
     children: [
       {
         path: '',
-        component: HomepageComponent
+        component: HomePageComponent
       }
     ]
   }

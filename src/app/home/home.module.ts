961ce33c78dae5f78003671d83bc217ecf8949ed
nnstsya <tonastyaket@gmail.com>
2025-01-08 16:24:22 +0200

    feat(FM-3.4) Implement customer review section
    - implemented customer review section with slider

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomePageComponent } from './home-page/home-page.component';
+import { ReviewsComponent } from './home-page/reviews/reviews.component';
+import { NgClass, NgOptimizedImage } from '@angular/common';
+import { ReviewsSliderComponent } from '@home/home-page/reviews-slider/reviews-slider.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
     HomePageComponent,
+    ReviewsComponent,
+    ReviewsSliderComponent
   ],
   imports: [
-    HomeRoutingModule
+    HomeRoutingModule,
+    NgOptimizedImage,
+    NgClass
   ]
 })
 export class HomeModule {}

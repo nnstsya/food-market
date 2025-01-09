280ef03d8fc300ee9f77ecc6d3832a6019b73d4e
nnstsya <tonastyaket@gmail.com>
2025-01-09 12:58:06 +0200

    feat(FM-3.5) Implement "Read our Blog posts" section
    - implemented recent blogs section, created reusable blog card component with 2 variations

--- src/app/home/home-page/home-page.component.ts
+++ src/app/home/home-page/home-page.component.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { blogData } from '@core/mocks/blogs';
+import { Blog } from '@core/models/blog.model';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
-export class HomePageComponent {}
+export class HomePageComponent implements OnInit {
+  blogs: Blog[] = blogData;
+  sortedBlogs: Blog[] = [];
+
+  ngOnInit() {
+    this.getSortedBlogs();
+  }
+
+  getSortedBlogs() {
+    this.sortedBlogs = this.blogs.sort((blog1, blog2) => {
+      const date1 = new Date(blog1.date.split('.').reverse().join('-'));
+      const date2 = new Date(blog2.date.split('.').reverse().join('-'));
+
+      return date2.getTime() - date1.getTime();
+    });
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,24 @@
 import { NgModule } from '@angular/core';
 
 import { DropdownComponent } from './components/dropdown/dropdown.component';
-import { NgOptimizedImage } from '@angular/common';
+import { NgOptimizedImage, NgStyle } from '@angular/common';
+import { BannerComponent } from '@shared/components/banner/banner.component';
+import { BlogCardComponent } from './components/blog-card/blog-card.component';
 
 @NgModule({
   declarations: [
-    DropdownComponent
+    DropdownComponent,
+    BannerComponent,
+    BlogCardComponent
   ],
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage,
+    NgStyle
   ],
   exports: [
-    DropdownComponent
+    DropdownComponent,
+    BannerComponent,
+    BlogCardComponent
   ]
 })
 export class SharedModule {}

c4e6c7a4481e623d522e27491de68c68996486ca
nnstsya <tonastyaket@gmail.com>
2025-01-15 15:08:13 +0200

    feat(FM-4.1) User auth modal
    - created custom modal component, added login and password recovery variants to it.
    - ensured login and password reset functionality, validations and error handling.

---
+++ src/app/auth/password-recovery/password-recovery.component.ts
@@ -0,0 +1,75 @@
+import { Component, DestroyRef, inject } from '@angular/core';
+import { FormGroup, NonNullableFormBuilder, Validators } from "@angular/forms";
+import { ModalService } from "@shared/components/modal/modal.service";
+import { AuthService } from "@auth/services/auth.service";
+import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
+import { catchError } from "rxjs";
+import { UserPasswordRecovery } from "@auth/models/user.model";
+import {PasswordRecoveryForm} from "@auth/models/form.model";
+
+@Component({
+  selector: 'app-password-recovery',
+  templateUrl: './password-recovery.component.html',
+  styleUrl: '../../shared/components/modal/modal.component.scss'
+})
+export class PasswordRecoveryComponent {
+  newPasswordVisible: boolean = false;
+  repeatedPasswordVisible: boolean = false;
+  passwordRecoveryForm: FormGroup<PasswordRecoveryForm>;
+  serverError: string = '';
+
+  private modalService: ModalService = inject(ModalService);
+  private authService: AuthService = inject(AuthService);
+  private destroyRef: DestroyRef = inject(DestroyRef);
+  private fb: NonNullableFormBuilder = inject(NonNullableFormBuilder);
+
+  constructor() {
+    this.passwordRecoveryForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      newPassword: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.pattern(/^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*();:'",./|])[A-Za-z\d!@#$%^&*();:'",./|]{6,}$/)
+        ]
+      ],
+      repeatedPassword: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.pattern(/^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*();:'",./|])[A-Za-z\d!@#$%^&*();:'",./|]{6,}$/)
+        ]
+      ]
+    });
+  }
+
+  toggleNewPasswordVisibility(): void {
+    this.newPasswordVisible = !this.newPasswordVisible;
+  }
+
+  toggleRepeatedPasswordVisibility(): void {
+    this.repeatedPasswordVisible = !this.repeatedPasswordVisible;
+  }
+
+  onSubmit(): void {
+    if (this.passwordRecoveryForm.valid) {
+      const user: UserPasswordRecovery = {...this.passwordRecoveryForm.getRawValue()};
+
+      if (user.newPassword === user.repeatedPassword) {
+        this.authService.resetPassword(user).pipe(
+          takeUntilDestroyed(this.destroyRef),
+          catchError((err: Error) => this.serverError = err.message)
+        ).subscribe({
+          next: (() => {
+            this.serverError = '';
+            this.modalService.hideModal();
+          })
+        });
+      } else {
+        this.serverError = 'Passwords must be identical. Please ensure both fields match.'
+      }
+    }
+  }
+}
---
+++ src/app/shared/components/button/button.component.ts
@@ -0,0 +1,12 @@
+import { Component, input, InputSignal } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss'
+})
+export class ButtonComponent {
+  variant: InputSignal<'secondary' | 'outlined' | 'basic'> = input<'secondary' | 'outlined' | 'basic'>('basic');
+  size: InputSignal<'s' | 'lg'> = input<'s' | 'lg'>('lg');
+  disabled: InputSignal<boolean> = input<boolean>(false);
+}

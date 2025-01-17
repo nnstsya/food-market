9ee9527d402f7d418f19a3d0a139bf88130ffc6a
nnstsya <tonastyaket@gmail.com>
2025-01-17 11:24:19 +0200

    feat(FM-3.1) implement home categories and promotions section
    - list component is now reusable, changed button component based on review comment

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -3,9 +3,10 @@ import { Component, input, InputSignal } from '@angular/core';
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
-  styleUrl: './button.component.scss'
+  styleUrl: './button.component.scss',
 })
 export class ButtonComponent {
-  secondary: InputSignal<boolean> = input<boolean>(false);
-  outlined: InputSignal<boolean> = input<boolean>(false);
+  variant: InputSignal<'basic' | 'secondary' | 'outlined'> = input<
+    'basic' | 'secondary' | 'outlined'
+  >('basic');
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,19 +1,24 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { BannerComponent } from './components/banner/banner.component';
 import { ButtonComponent } from './components/button/button.component';
+import { ListComponent } from './components/list/list.component';
 
 @NgModule({
   declarations: [
     ButtonComponent,
-    BannerComponent
+    BannerComponent,
+    ListComponent
   ],
   exports: [
     BannerComponent,
-    ButtonComponent
+    ButtonComponent,
+    ListComponent
   ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    NgOptimizedImage
+  ],
 })
-export class SharedModule { }
+export class SharedModule {}

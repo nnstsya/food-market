62db27da43fb1951f1ebf7131998139a4be2e1a7
Nastya <139653499+nnstsya@users.noreply.github.com>
2025-01-17 10:58:58 +0200

    feat(FM-3.4) Implement customer review section (#7)
    - implemented customer review section with slider

--- src/app/home/home-page/home-page.component.ts
+++ src/app/home/home-page/home-page.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { Review } from '@core/models/review.model';
+import { reviewData } from '@core/mocks/reviews';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
-export class HomePageComponent {}
+export class HomePageComponent {
+  reviews: Review[] = reviewData;
+}
---
+++ src/app/home/home-page/reviews-slider/reviews-slider.component.ts
@@ -0,0 +1,59 @@
+import { Component, input, InputSignal, OnInit } from '@angular/core';
+import { Review } from '@core/models/review.model';
+
+@Component({
+  selector: 'app-reviews-slider',
+  templateUrl: './reviews-slider.component.html',
+  styleUrls: ['./reviews-slider.component.scss'],
+})
+export class ReviewsSliderComponent implements OnInit {
+  reviews: InputSignal<Review[]> = input.required<Review[]>();
+
+  visibleReviews: Review[] = [];
+  reviewIndex: number = 0;
+  slideDirection: string = '';
+  isTransitioning: boolean = false;
+
+  ngOnInit() {
+    this.updateSlides();
+  }
+
+  updateSlides() {
+    const length: number = this.reviews().length;
+    const visibleSlideCount = 6;
+
+    const indices: number[] = Array.from({ length: visibleSlideCount }, (_, i) => {
+      const offset: number = i - Math.floor(visibleSlideCount / 3);
+      return (this.reviewIndex + offset + length) % length;
+    });
+
+    this.visibleReviews = indices.map(index => this.reviews()[index]);
+
+    this.slideDirection = '';
+    this.isTransitioning = false;
+  }
+
+  getPrev() {
+    if (this.isTransitioning) return;
+    this.isTransitioning = true;
+
+    this.reviewIndex = (this.reviewIndex - 1 + this.reviews().length) % this.reviews().length;
+    this.slideDirection = 'prev';
+
+    setTimeout(() => {
+      this.updateSlides();
+    }, 600);
+  }
+
+  getNext() {
+    if (this.isTransitioning) return;
+    this.isTransitioning = true;
+
+    this.reviewIndex = (this.reviewIndex + 1) % this.reviews().length;
+    this.slideDirection = 'next';
+
+    setTimeout(() => {
+      this.updateSlides();
+    }, 600);
+  }
+}

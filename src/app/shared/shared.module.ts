059541e5e09df35322ec8e18a4602ba92fc1432d
nnstsya <tonastyaket@gmail.com>
2025-01-23 19:11:47 +0200

    feat(FM-5.3) Implemented pagination component
    - implemented reusable pagination component with "show more" button

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -9,6 +9,7 @@ import { RouterLink } from "@angular/router";
 import { ButtonComponent } from "@shared/components/button/button.component";
 import { ProductCardComponent } from "@shared/components/product-card/product-card.component";
 import { TagComponent } from './components/tag/tag.component';
+import { PaginationComponent } from './components/pagination/pagination.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { TagComponent } from './components/tag/tag.component';
     BreadcrumbsComponent,
     ButtonComponent,
     ProductCardComponent,
-    TagComponent
+    TagComponent,
+    PaginationComponent
   ],
   imports: [
     NgOptimizedImage,
@@ -35,7 +37,8 @@ import { TagComponent } from './components/tag/tag.component';
     BreadcrumbsComponent,
     ButtonComponent,
     ProductCardComponent,
-    TagComponent
+    TagComponent,
+    PaginationComponent
   ]
 })
 export class SharedModule {}

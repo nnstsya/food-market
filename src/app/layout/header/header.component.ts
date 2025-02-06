16db8c485ee9689cce74344c8fc784df99d0259f
nnstsya <tonastyaket@gmail.com>
2025-02-06 14:17:55 +0200

    feat(FM-5.5) Implement category tabs
    - each tab is now clickable, active tab differs from others
    - navigation bar is now scrolling on smaller screens
    - category page now has fallback placeholder
    - category page now has loader while fetching products data

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
-import { CategoryItem } from '@core/models/category.model';
+import { Category, CategoryItem } from '@core/models/category.model';
 import { categoryData } from '@core/mocks/categories';
 
 @Component({
@@ -17,5 +17,15 @@ export class HeaderComponent {
 
   constructor() {
     this.isHomePage = this.router.url === '/'
+  }
+
+  getCategoryName(title: string): string {
+    const categoryKey = title.split(' ').join('').toUpperCase() as keyof typeof Category;
+
+    return Category[categoryKey].toLowerCase();
+  }
+
+  checkIfTabActive(title: string) {
+    return this.router.url.split('/').includes(title.split(' ').join('').toLowerCase());
   }
 }
